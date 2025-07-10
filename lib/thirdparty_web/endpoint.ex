79b610fab27d57cc1c8de5dfa85bcf911c38f510
Rohan87080 <rohan@fairtechsol.com>
2025-07-10 11:35:51 +0530

    Add CORS support by including CORSPlug in the endpoint and project dependencies

--- lib/thirdparty_web/endpoint.ex
+++ lib/thirdparty_web/endpoint.ex
@@ -20,6 +20,8 @@ defmodule ThirdpartyWeb.Endpoint do
     longpoll: [connect_info: [session: @session_options]]
   )
 
+  plug(CORSPlug, origin: "*")
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Thirdparty.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:cors_plug, "~> 3.0"},
       {:phoenix, "~> 1.7.21"},
       {:cachex, "~> 3.5"},
       {:phoenix_ecto, "~> 4.5"},
